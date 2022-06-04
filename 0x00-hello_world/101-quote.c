897527c7b87c72f738d84b7bd5d03d240b2548af
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-04 00:58:01 -0700

    UNIX is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
-* main - Entry point
-* Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 1.
+ */
+
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1 59);
+	return (1);
 }
