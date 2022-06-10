65f1aa3327e5da3a6d98aa9e031733397e6d146e
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-09 21:58:56 -0700

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle
+ * @size: takes in an integer for size of triangle.
+ */
+void print_triangle(int size)
+{
+	int row, column, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = size - row; column > 1; column--)
+		{
+			_putcahr(' ');
+		}
+		for (k = row + column; k >= 1; k--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
