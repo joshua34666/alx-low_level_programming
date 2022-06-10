63ace12fb2838946d508e8ba60a7544304ac9dae
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-09 21:29:43 -0700

    Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,17 +15,15 @@ void print_square(int size)
 
 	if (size > 0)
 	{
+		_putchar('\n')
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar(square);
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
