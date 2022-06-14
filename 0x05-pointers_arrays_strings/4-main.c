86480e4232f4fc5e2ccad297fa785f9e24cf7427
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-13 20:45:28 -0700

    Print Reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check for the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. i fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
