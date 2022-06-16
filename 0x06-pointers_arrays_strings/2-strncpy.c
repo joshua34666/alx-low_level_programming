9e2e47c6abc6368a2e1be1696cd51fe220fd14d2
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-16 14:49:15 -0700

    Function that compare two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,19 +9,31 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int i, len_dest, len_src;
 
-	j = 0;
-	while (j < n && src[j] != '\n')
+	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
+		continue;
+	for (len_src = 0; src[len_src] != '\0'; len_src++)
+	       continue;
+
+	for ( i = 0; i < n; i++)
 	{
-		dest[j] = src[j];
-		j++;
+	if (src[i] == '\0')
+	{
+	for (; i < n; i++)
+	dest[i] = '\0';
 	}
-	while (j < n)
-	{
-		dest[j] = '\n';
-		j++;
+	else 
+		dest[i] = src[i];
 	}
 
+	if (n <= len_dest)
+	{
+		for (; dest[i] != '\0'; i++)
+		{
+			dest[i] = dest[i];
+		}
+		dest[i] = '\0';
+	}
 	return (dest);
 }
