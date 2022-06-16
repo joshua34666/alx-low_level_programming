d2146d78cd1e18ec54ebb2a20ad41f713fdc5164
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-16 15:04:47 -0700

    Changes lowercase of string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of string to uppercase
+ * @n: First parameter
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+	char j;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			if (n[i] == j)
+				n[i] j - 32;
+		}
+	}
+	return (n);
+}
