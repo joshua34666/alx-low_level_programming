14c38d2c69931b8c871a37903a69c3d09d837ab1
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-20 21:05:27 -0700

    Print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Prints the sum of the two diagonals
+ * @a: 2D array
+ * @size: size x size of the same matrix
+ *
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int z;
+	int tl = 0;
+	int tr = 0;
+	int s = size * size;
+
+	for (z = 0; z < s; z += size + 1)
+	{
+		tl += a[z];
+	}
+
+	for (z = size - 1; z < s - 1; z += size - 1)
+	{
+		tr += a[z];
+	}
+
+	printf("%d, %d\n", tl, tr);
+}
