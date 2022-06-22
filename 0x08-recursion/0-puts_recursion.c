6d7eebf32836e6b9108395f7053cae2e35617445
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-06-22 14:15:04 -0700

    String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line.
+ * @s: points to string
+ *
+ * Return: 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
