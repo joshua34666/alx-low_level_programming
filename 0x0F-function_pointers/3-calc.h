bde1ded37697b85fabfc4c3a4df07d84b0f87e4b
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-07-06 19:38:22 -0700

    A programme to perform simple calculation

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,24 @@
+#ifndef OPERATIONS
+#define OPERATIONS
+/**
+ * struct_op - struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	{
+		char *op;
+		int (*f)(int y, int x);
+		op_t;
+	}
+
+	int op_add(int y, int x);
+	int op_sub(int y, int x);
+	int op_mul(int y, int x);
+	int op_div(int y, int x);
+	int op_mod(int y, int x);
+	int (*get_op_func(char *s))(int, int);
+
+#endif
+}
