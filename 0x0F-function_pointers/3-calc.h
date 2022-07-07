ac7e6d29f171295142459d67739e3fc9f6fb18dc
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-07-06 20:09:28 -0700

    Perform simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef _CALC_H_
+#define _CALC_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,17 +10,15 @@
  */
 typedef struct op
 {
-	{
-		char *op;
-		int (*f)(int y, int x);
-	} op_t;
+	char *op;
+	int (*f)(int y, int x);
+} op_t;
 
-	int op_add(int y, int x);
-	int op_sub(int y, int x);
-	int op_mul(int y, int x);
-	int op_div(int y, int x);
-	int op_mod(int y, int x);
-	int (*get_op_func(char *s))(int, int);
+int op_add(int y, int x);
+int op_sub(int y, int x);
+int op_mul(int y, int x);
+int op_div(int y, int x);
+int op_mod(int y, int x);
+int (*get_op_func(char *s))(int, int);
 
 #endif
-}
