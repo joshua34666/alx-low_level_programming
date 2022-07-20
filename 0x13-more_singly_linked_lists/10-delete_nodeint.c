2338900ce0ccfc09c00103c007b7a30b74837385
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-07-19 20:54:06 -0700

    Delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,26 +10,29 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *current, *next;
-if (head == NULL || *head == NULL)
-return (-1);
-if (index == 0)
-{
-next = (*head)->next;
-free(*head);
-*head = next;
-return (1);
-}
-current = *head;
-for (i = 0; i < index - 1; i++)
-{
-if (current->next == NULL)
-return (-1);
-current = current->next;
-}
-next = current->next;
-current->next = next->next;
-free(next);
-return (1);
+	unsigned int j;
+	listint_t *x, *y;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		y = (*head)->y;
+		free(*head);
+		*head = y;
+		return (1);
+	}
+
+	x = *head;
+	for (j = 0; j < index - 1; j++)
+	{
+		if (x->y == NULL)
+			return (-1);
+		x = x->y;
+	}
+	y = x->y;
+	x->y = y->y;
+	free(y);
+
+	return (1);
 }
