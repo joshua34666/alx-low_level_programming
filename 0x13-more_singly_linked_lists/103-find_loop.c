a09e93867d1a465905764b6d9fd168a9216e1a78
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-07-19 21:20:27 -0700

    Finds a loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,28 +10,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *a, *b;
-
-	if (head == NULL || head->next == NULL)
-		return (NULL);
-
-	a = head->next;
-	b = (head->next)->next;
-	while (b)
-	{
-		if (a == b)
-		{
-			a = head;
-			while (a != b)
-			{
-				b = a->next;
-				b = b->next;
-			}
-			return (a);
-		}
-		a = a->next;
-		b = (b->next)->next;
-	}
-
-	return (NULL);
+listint_t *i, *j;
+if (head == NULL || head->next == NULL)
+return (NULL);
+i = head->next;
+j = (head->next)->next;
+while (j)
+{
+if (i == j)
+{
+i = head;
+while (i != j)
+{
+i = i->next;
+j = j->next;
+}
+return (i);
+}
+i = i->next;
+j = (j->next)->next;
+}
+return (NULL);
 }
