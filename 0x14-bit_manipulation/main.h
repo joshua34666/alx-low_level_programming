74e1eac26ce78e8b74e65828b03c8f8b32538fb3
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-07-21 22:39:57 -0700

    work hard

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,7 +8,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int check_password(char *pwd);
 int _putchar(char c);
 
 #endif
