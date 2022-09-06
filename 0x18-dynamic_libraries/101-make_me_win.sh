324728deea8c8562617971627bd4b24fee06b9de
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-09-05 18:23:31 -0700

    Make it happen

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/joshua34666/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+d52e6c18e0723f5b025a75dea19ef365  gm
 export LD_PRELOAD="$PWD/../libgiga.so"
